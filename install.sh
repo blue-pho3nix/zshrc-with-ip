22bb8f5b494f40b11bf3d6e97b7d95319daf0cb1
Jackie Friedberg <148140471+Pho3nix-Jacki3@users.noreply.github.com>
2024-03-25 18:40:05 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+#update
+sudo apt-get update
+sudo apt-get --fix-missing && sudo apt update
+
+#install ness nerdfonts
+mkdir -p ~/.local/share/fonts/
+
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/RobotoMono.zip
+
+unzip RobotoMono.zip -d ~/.local/share/fonts/
+
+fc-cache -fv
+
+#copy .zshrc file incase you want to revert
+mv ~/.zshrc ~/.zshrc-cp
+
+#get new .zshrc file 
+cd ~/ && wget https://raw.githubusercontent.com/Pho3nix-Jacki3/zshrc-with-ip/main/.zshrc
+
+source ~/.zshrc
