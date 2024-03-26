c4b9bdc869bc5e53824ae29ad0aa2a611888da4b
Jackie Friedberg <148140471+Pho3nix-Jacki3@users.noreply.github.com>
2024-03-26 08:25:44 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,8 +12,8 @@ unzip RobotoMono.zip -d ~/.local/share/fonts/
 
 fc-cache -fv
 
-#copy .zshrc file incase you want to revert
-mv ~/.zshrc ~/.zshrc-cp
+# move .zshrc file incase you want to revert
+mv ~/.zshrc ~/.zshrc-old
 
 #get new .zshrc file 
 cd ~/ && wget https://raw.githubusercontent.com/Pho3nix-Jacki3/zshrc-with-ip/main/.zshrc
